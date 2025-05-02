c28c8c795f9926cd6e7c98f7dc55ccf717ddffde
obeyhades <abdu.nazar145@gmail.com>
2025-05-02 11:38:33 +0200

    setting up the project

---
+++ src/app/pages/start-site/start-site.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-start-site',
+  imports: [],
+  templateUrl: './start-site.component.html',
+  styleUrl: './start-site.component.css'
+})
+export class StartSiteComponent {
+
+}
