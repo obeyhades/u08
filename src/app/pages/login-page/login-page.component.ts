b79af47f10f2325623596e7c4b1681f0101d2ae2
obeyhades <abdu.nazar145@gmail.com>
2025-05-06 10:25:29 +0200

    feat: working on it!

--- src/app/pages/login-page/login-page.component.ts
+++ src/app/pages/login-page/login-page.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.css'
 })
 export class LoginPageComponent {
-
 }
