691ee6e05e94a7cdf27a5b380c332b29b86c3abb
obeyhades <abdu.nazar145@gmail.com>
2025-05-06 10:23:34 +0200

    feat: add routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { StartSiteComponent } from './pages/start-site/start-site.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { RegisterPageComponent } from './pages/register-page/register-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', component: StartSiteComponent},
+  {path: 'login', component: LoginPageComponent},
+  {path: 'register', component:RegisterPageComponent},
+
+
+];
+
