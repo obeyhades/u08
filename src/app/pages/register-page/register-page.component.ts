7ebf07ebfb6e3266d3f508b92b599beb95719f63
obeyhades <abdu.nazar145@gmail.com>
2025-05-07 10:04:23 +0200

    feat:  add html and styles for register page

--- src/app/pages/register-page/register-page.component.ts
+++ src/app/pages/register-page/register-page.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { StartBgComponent } from "../../components/start-bg/start-bg.component";
+
 
 @Component({
   selector: 'app-register-page',
-  imports: [],
+  imports: [StartBgComponent],
   templateUrl: './register-page.component.html',
   styleUrl: './register-page.component.css'
 })
