8f915359c38b47c2e3f4194870410bc31d5b1ca2
obeyhades <abdu.nazar145@gmail.com>
2025-05-12 18:12:21 +0200

    feat: add navbar to mainpage

--- src/app/pages/main-page/main-page.component.ts
+++ src/app/pages/main-page/main-page.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from "../../components/navbar/navbar.component";
+
 
 @Component({
   selector: 'app-main-page',
-  imports: [],
+  imports: [NavbarComponent],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.css'
 })
