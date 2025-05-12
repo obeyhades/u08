c1b2193657384d39d6949285c93a63a95eeda312
obeyhades <abdu.nazar145@gmail.com>
2025-05-12 22:30:49 +0200

    feat: add the edit component

--- src/app/pages/edit-profile/edit-profile.component.ts
+++ src/app/pages/edit-profile/edit-profile.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from "../../components/navbar/navbar.component";
+import { StartBgComponent } from "../../components/start-bg/start-bg.component";
+import { EditprofileBgComponent } from "../../components/editprofile-bg/editprofile-bg.component";
 
 @Component({
   selector: 'app-edit-profile',
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, StartBgComponent, EditprofileBgComponent],
   templateUrl: './edit-profile.component.html',
   styleUrl: './edit-profile.component.css'
 })
