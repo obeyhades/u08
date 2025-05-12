e6c0925b0bf12c1c164d77001366eae049408012
obeyhades <abdu.nazar145@gmail.com>
2025-05-12 18:11:03 +0200

    feat: add editprofile router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { StartSiteComponent } from './pages/start-site/start-site.component';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { RegisterPageComponent } from './pages/register-page/register-page.component';
 import { MainPageComponent } from './pages/main-page/main-page.component';
+import { EditProfileComponent } from './pages/edit-profile/edit-profile.component';
 
 export const routes: Routes = [
   {path: '', component: StartSiteComponent},
   {path: 'login', component: LoginPageComponent},
   {path: 'register', component:RegisterPageComponent},
   {path: 'mainpage', component:MainPageComponent},
+  {path: 'editProfile', component:EditProfileComponent}
 ];
 
