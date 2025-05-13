e2708f586ef99bdf98fc5e930b96fffe81e273c8
obeyhades <abdu.nazar145@gmail.com>
2025-05-13 15:59:49 +0200

    feat: add url and httpClient

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,9 +1,27 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+export interface User {
+  id: number;
+  username: string;
+  email: string;
+  password: string;
+  }
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+  private url = 'http://localhost:8080' //'https://moneytracker-hw96.onrender.com';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getUsers(): Observable<User[]> {
+    return this.http.get<User[]>(`${this.url}/user`);
+  }
+
+  logIn(credentials: {username: string, password: string}): Observable<User> {
+    return this.http.post<User>(`${this.url}/user/login`,credentials);
+  }
 }
