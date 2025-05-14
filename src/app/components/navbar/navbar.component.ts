910af3dfa1acc6fe909cf064df956669e6a3d0ae
obeyhades <abdu.nazar145@gmail.com>
2025-05-14 19:44:37 +0200

    feat: add dynamik navbar setting

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-navbar',
@@ -8,5 +9,11 @@ import { RouterLink } from '@angular/router';
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
-
+  userId: number = 0;
+  constructor(private userService: UserService) {
+    this.userService.userId$.subscribe((id: number) => {
+      this.userId = id;
+    })
+   }
 }
+ 
